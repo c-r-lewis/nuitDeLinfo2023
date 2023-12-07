d9fd44159764f1c24c438be7826d18a160e30e23
Lewis <charlotte.rose.lewis.04@gmail.com>
2023-12-07 23:55:46 +0100

    feat: scroll to change view

--- scripts/zoom.js
+++ scripts/zoom.js
@@ -1,3 +1,15 @@
 function zoomIn(element) {
+    element.textContent = "Clicker pour dezoomer";
     element.classList.toggle("zoomed");
 }
+
+function changeView(idSection) {
+    const activeSection = document.querySelector(".active");
+    const newActiveElement = document.getElementById(idSection);
+    newActiveElement.classList.add("active");
+
+    newActiveElement.scrollIntoView({ behavior: 'smooth' });
+
+    activeSection.classList.remove("active");
+}
+
