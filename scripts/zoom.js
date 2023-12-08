7b10dc5ad147de2923fce54ba25bb98f9d5bc2d3
Lewis <charlotte.rose.lewis.04@gmail.com>
2023-12-08 05:57:37 +0100

    feat: posters on board

--- scripts/zoom.js
+++ scripts/zoom.js
@@ -2,13 +2,18 @@ function zoomIn(element) {
     if (element.classList.contains("zoomed")) {
         element.classList.remove("zoomed");
         element.src = '../ressources/images/board.png';
+        document.querySelectorAll(".poster").forEach(poster => {
+            poster.style.display = "none";
+        });
     }
     else {
+        document.querySelectorAll(".poster").forEach(poster => {
+            poster.style.display = "block";
+        });
         element.classList.add("zoomed");
         element.src = '../ressources/images/board2.png';
+
     }
-
-
 }
 
 function changeView(idSection) {
