b49f81d99b9e9d96fe8161cf91a4a11051a3007f
Lewis <charlotte.rose.lewis.04@gmail.com>
2023-12-08 07:33:23 +0100

    git commit panneau

--- scripts/animation.js
+++ scripts/animation.js
@@ -2,11 +2,13 @@
 function zoomIn(element) {
     if (element.classList.contains("zoomed")) {
         element.classList.remove("zoomed");
+        document.getElementById("poster").style.display = "none";
         element.src = "../ressources/images/board.png";
     }
     else {
         element.src = "../ressources/images/board2.png";
         element.classList.add("zoomed");
+        document.getElementById("poster").style.display = "block";
     }
 }
 
@@ -14,7 +16,6 @@ function changeView(idSection) {
     const activeSection = document.querySelector(".active");
     const newActiveElement = document.getElementById(idSection);
     newActiveElement.classList.add("active");
-
     newActiveElement.scrollIntoView({ behavior: 'smooth' });
 
     activeSection.classList.remove("active");
