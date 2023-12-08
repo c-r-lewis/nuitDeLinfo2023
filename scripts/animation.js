98c38e22c6dcb9b8eb83417c1ef337e36f7bacc4
mickael <mickaelcoulon34570@gmail.com>
2023-12-08 01:26:19 +0100

    Ajout du TypeWriter dans animation.js pour écrire un texte défilant

--- scripts/animation.js
+++ scripts/animation.js
@@ -1,3 +1,9 @@
+new Typewriter(".typewriter", {})
+    .changeDelay(40)
+    .typeString("J'ai compris") //dans ce typeString, il faut écrire le texte (donc en utilisant ce que fait katia)
+    .start();
+//Pour appliquer ceci à un élément, il faut rajouter class = "typewriter" dans l'élément
+
 function zoomIn(element) {
     element.textContent = "Clicker pour dezoomer";
     element.classList.toggle("zoomed");
